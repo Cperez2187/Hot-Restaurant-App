0e0c966634ba6ce34736c72bc2f83479546e1283
cperez2187 <cperez2187@gmail.com>
2017-07-15 11:18:59 -0700

    modified app.js

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+
 // Set up Express app
 // =======================================
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -18,3 +20,4 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({
   type: "application/vnd.api+json"
 }));
+
